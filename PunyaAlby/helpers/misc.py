db50358ff65691d9c50cef9d9ed7020db167ba06
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-06-16 13:27:14 +0700

    Update and rename ProjectMan/helpers/misc.py to PunyaAlby/helpers/misc.py

--- PunyaAlby/helpers/misc.py
+++ PunyaAlby/helpers/misc.py
@@ -17,7 +17,7 @@ from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError
 
 from config import *
-from ProjectMan import LOGGER
+from PunyaAlby import LOGGER
 
 HAPP = None
 
@@ -68,9 +68,9 @@ def git():
         UPSTREAM_REPO = REPO_URL
     try:
         repo = Repo()
-        LOGGER("ProjectMan").info(f"Git Client Found")
+        LOGGER("PunyaAlby").info(f"Git Client Found")
     except GitCommandError:
-        LOGGER("ProjectMan").info(f"Invalid Git Command")
+        LOGGER("PunyaAlby").info(f"Invalid Git Command")
     except InvalidGitRepositoryError:
         repo = Repo.init()
         if "origin" in repo.remotes:
@@ -95,7 +95,7 @@ def git():
         except GitCommandError:
             repo.git.reset("--hard", "FETCH_HEAD")
         install_req("pip3 install --no-cache-dir -U -r requirements.txt")
-        LOGGER("ProjectMan").info("Fetched Latest Updates")
+        LOGGER("PunyaAlby").info("Fetched Latest Updates")
 
 
 def is_heroku():
@@ -109,8 +109,8 @@ def heroku():
             try:
                 Heroku = heroku3.from_key(HEROKU_API_KEY)
                 HAPP = Heroku.app(HEROKU_APP_NAME)
-                LOGGER("ProjectMan").info(f"Heroku App Configured")
+                LOGGER("PunyaAlby").info(f"Heroku App Configured")
             except BaseException:
-                LOGGER("ProjectMan").warning(
+                LOGGER("PunyaAlby").warning(
                     f"Pastikan HEROKU_API_KEY dan HEROKU_APP_NAME anda dikonfigurasi dengan benar di config vars heroku."
                 )
