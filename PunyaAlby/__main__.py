e53a1658092ef60ab296eefa91c3f844f1b4ecf6
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-06-18 14:57:39 +0700

    Update __main__.py

--- PunyaAlby/__main__.py
+++ PunyaAlby/__main__.py
@@ -27,8 +27,9 @@ async def main():
         try:
             await bot.start()
             bot.me = await bot.get_me()
-            await bot.join_chat("Lunatic0de")
-            await bot.join_chat("SharingUserbot")
+            await bot.join_chat("ruangdiskusikami")
+            await bot.join_chat("ruangprojects")
+            await bot.join_chat("ruang_gabutku")
             await bot.send_message(BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER))
         except Exception as a:
             LOGGER("main").warning(a)
