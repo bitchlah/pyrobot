03314e1c105735b2c8609f88cad3a4c0d202c0e3
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-06-20 16:02:49 +0700

    Update alive.py

--- PunyaAlby/modules/alive.py
+++ PunyaAlby/modules/alive.py
@@ -14,7 +14,7 @@ from pyrogram import __version__ as jembut
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from config import LOGO_ALBY, PREFIX
+from config import PREFIX
 from PunyaAlby import CMD_HELP, StartTime
 
 CMD_HELP.update(
@@ -72,7 +72,6 @@ async def alive(_, m):
     reply_msg += f"► ᴏᴡɴᴇʀ: [{eek}](tg://user?id={berak})\n"
     time.time()
     reply_msg += f"► Prime uptime: {uptime}\n└───────═━┈━═──────"
-    photo = LOGO_ALBY
     await m.delete()
     if m.reply_to_message:
         await client.send_photo(
