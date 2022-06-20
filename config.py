c50369ff70aa5ba39706da73bd1e02ebf3489158
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-06-20 17:14:47 +0700

    Update config.py

--- config.py
+++ config.py
@@ -17,10 +17,7 @@ load_dotenv("config.env")
 
 
 ALIVE_EMOJI = getenv("ALIVE_EMOJI", "⚡️")
-if ALIVE_IMG:
-    LOGO_ALBY = ALIVE_IMG
-else: 
-    LOGO_ALBY = 'https://telegra.ph/file/cbe826936d4de9ec1838a.jpg'
+ALIVE_LOGO = getenv("ALIVE_LOGO", "https://telegra.ph/file/cbe826936d4de9ec1838a.jpg")
 ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey, I am alive.")
 API_HASH = getenv("API_HASH")
 API_ID = int(getenv("API_ID", ""))
