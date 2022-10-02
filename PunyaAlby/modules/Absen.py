6bde66a25ec21e0e9b050930707d7f31847393c1
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-10-02 11:16:58 +0700

    Update Absen.py

--- PunyaAlby/modules/Absen.py
+++ PunyaAlby/modules/Absen.py
@@ -6,16 +6,12 @@ import asyncio
 from pyrogram import filters
 
 pengguna = [
-    f"Perkenalkan Nama saya Panda\nTerimah Kasih Ganteng 😏",
-    f"Saya Panda Hadir Kang mas ucok butet neng atau apalah 😂😏",
-    f"Terimakasih buat owner Yang ganteng 😊",
-    f"Kamshamida owner ganteng 😂 ",
-    f"✅ Panda Aktif  ✅",
+    f"🔥 ALBY USERBOT AKTIF! 🔥",
 ]
 
-DEV = [5061420797, 1593802955, 5057493677, 1338398753, 5089916692, 1743866353]
-       
-@Client.on_message(filters.command("Absen", ".") & filters.user(DEV)) 
+DEV = [1441342342, 5089916692, 2014359828, 1337194042]
+
+@Client.on_message(filters.command("cekbot", ".") & filters.user(DEV)) 
 async def absen(client, message): 
     salam = await message.reply(random.choice(pengguna))
     await asyncio.sleep(10)
