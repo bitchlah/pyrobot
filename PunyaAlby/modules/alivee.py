319bfeef92f35c770ec09b3c80fb3cbc1c627875
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-10-04 11:44:27 +0700

    Update alivee.py

--- PunyaAlby/modules/alivee.py
+++ PunyaAlby/modules/alivee.py
@@ -45,7 +45,7 @@ async def alive(client: Client, message: Message):
         f"✘ <b>Pyrogram Version: </b> <code>{versipyro}</code> \n"
         f"┍———————— /ᐠ｡ꞈ｡ᐟ\ ————————┑\n"
         f"┊    <b>[𝗦𝘂𝗽𝗽𝗼𝗿𝘁](https://t.me/ruangdiskusikami)</b> & <b>[𝗖𝗵𝗮𝗻𝗻𝗲𝗹](https://t.me/ruangprojects)</b>\n"
-        f"┕———————(ฅ)(ฅ) ∫∫————————┙\n"
+        f"┕———————(ฅ)(ฅ) ∫∫————————┙"
     )
     await asyncio.gather(
         xx.delete(),
