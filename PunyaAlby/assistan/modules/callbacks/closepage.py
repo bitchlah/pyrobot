7a01ab0258283df664212c84210e0e233ee0630d
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-11-15 23:11:00 +0700

    Create closepage.py

---
+++ PunyaAlby/assistan/modules/callbacks/closepage.py
@@ -0,0 +1,38 @@
+"""
+This file is for closed page inline help menu.
+"""
+
+from pyrogram import filters
+
+from pyrogram.types import (
+    InputMediaPhoto,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    CallbackQuery
+)
+
+from PunyaAlby import app
+
+
+
+
+
+@app.bot.on_callback_query(filters.regex("close-tab"))
+@app.alert_user
+async def _close(_, cb: CallbackQuery):
+    await cb.edit_message_media(
+        media=InputMediaPhoto(
+            media="app.ALIVE_LOGO()", 
+            caption=app.close_tab_string()
+        ),
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="OPEN",
+                        callback_data="home-tab"
+                    )
+                ]
+            ]
+        )
+    )
